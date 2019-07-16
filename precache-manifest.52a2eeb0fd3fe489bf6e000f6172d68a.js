4a3a7bd91eaf06f955c4c40ba7f8d1de0232a173
pyjcpp <pyjcpp@gmail.com>
2019-07-16 12:46:33 -0500

    Updates

---
+++ precache-manifest.52a2eeb0fd3fe489bf6e000f6172d68a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff4f1fd3072cc5ea323ad50dba53387",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c220032372a2f9090047",
+    "url": "/monsters-rolodex/static/css/main.0a9b6ca3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c220032372a2f9090047",
+    "url": "/monsters-rolodex/static/js/main.9b09b815.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
